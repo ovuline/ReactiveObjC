15b65ec75453e5c01c2dd6611d325d136fdd01c0
Cheng-Jie Heng <jie@tucali.ca>
2021-08-05 21:52:01 -0400

    Add Package.swift and delete unnecessary files/code

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "ReactiveObjC",
+    platforms: [.iOS(.v9)],
+    products: [
+        .library(name: "ReactiveObjC", type: .dynamic, targets: ["ReactiveObjC"])
+    ],
+    targets: [
+        .target(
+            name: "ReactiveObjC",
+            path: "ReactiveObjC",
+            publicHeadersPath: "."
+        )
+    ]
+)
